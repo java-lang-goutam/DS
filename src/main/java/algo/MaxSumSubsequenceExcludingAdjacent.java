4ba58f83b74a3d89214906172a115fbb92b12597
Goutam Kumar Singh <gou@adobe.com>
2022-02-08 23:45:55 +0530

    Theif neighbour problem

---
+++ src/main/java/algo/MaxSumSubsequenceExcludingAdjacent.java
@@ -0,0 +1,51 @@
+package algo;
+
+public class MaxSumSubsequenceExcludingAdjacent {
+
+    public static int find(int... arr) {
+        //return find_recursive(arr, 0);
+        return find_dp(arr, arr.length);
+    }
+
+    public static int find_dp(int[] arr, int n) {
+
+        // dp[i] : max sum sequence ending at i
+        int dp[] = new int[n];
+
+        // initialize value
+        dp[0] = arr[0];
+
+        for (int i = 1; i < n; i++) {
+
+            // including i
+            int incl = (i-2)>=0 ? dp[i - 2] + arr[i] : arr[i];
+
+            // excluding i
+            int excl = dp[i - 1];
+
+            dp[i] = Math.max(incl, excl);
+
+        }
+
+        return dp[n-1];
+    }
+
+    public static int find_recursive(int arr[], int i) {
+
+        if (i >= arr.length)
+            return 0;
+
+        // including i
+        int incl = arr[i] + find_recursive(arr, i + 2);
+
+        // excluding i
+        int excl = find_recursive(arr, i + 1);
+
+        return Math.max(incl, excl);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(find(6, 7, 1, 3, 8, 2, 4));
+        System.out.println(find(5, 3, 4, 11, 2));
+    }
+}
