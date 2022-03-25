7cfbe7fd3a8dc19367c08f82eee53e8812f52b77
Goutam Kumar Singh <gou@adobe.com>
2022-03-25 21:14:46 +0530

    Two Array Triplet

--- src/main/java/quirks/Quirks.java
+++
@@ -1,8 +0,0 @@
-package quirks;
-
-public class Quirks {
-    public static void main(String[] args) {
-        // System.out.println("Hello\u0022\u002e\u006c\u0065\u006e\u0067\u0074\u0068\u0028\u0029 + \u0022World".length());
-         System.out.println("Hello".length() + "World".length());
-    }
-}
