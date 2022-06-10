850dcfeef08e8ab7f7f18681a06732fcf6d75735
Goutam Kumar Singh <java.lang.goutam@gmail.com>
2022-06-10 16:21:36 +0530

    Add Restricted paths

---
+++ src/main/java/graph/RestrictedPaths.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class RestrictedPaths {
+
+	private class Node {
+		private int vertex, weight;
+		Node (final int vertex, final int weight) {
+			this.vertex = vertex;
+			this.weight = weight;
+		}
+	}
+
+	final List<List<Node>> graph;
+	final int[] parent;
+	final int INF = Integer.MAX_VALUE;
+	final int NO_PARENT = -1;
+
+    public int countRestrictedPaths(int n, int[][] edges) {       
+    	graph = new ArrayList<>();
+    	parent = new int[N];
+    }
+
+
+
+
+}
